c2a85f29448e6df641180d5419aebfef438bf420
Diya Sharma <sharmadiya7200@gmail.com>
2025-02-17 13:20:15 -0800

    Added SnapList

---
+++ src/components/SnapList/SnapList.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./SnapList.scss";
+import snapData from "../../data/photos.json";
+import SnapItem from "../SnapItem/SnapItem";
+
+function SnapList(props){
+
+    const [snap, setSnap] = useState(snapData);
+
+    let filteredSnap = snap;
+
+    if (props.selectedTag !== "") {
+
+        filteredSnap = snap.filter((snap) =>{
+            return snap.tags.includes(props.selectedTag);
+        })
+    }
+
+
+    return(
+        <section className="snap-list">
+            <ul className="snap-list__info">
+                {filteredSnap.map((snap) =>{
+                    return <SnapItem key={snap.id} snap={snap} />;
+                })}
+            </ul>
+        </section>
+    );
+}
+
+export default SnapList;
