4e6d8c9aa6397304b2d5d9f809ac4c1ff1fc3441
Diya Sharma <sharmadiya7200@gmail.com>
2025-02-17 22:35:58 -0800

    Changes made in SnapItem

--- src/components/SnapItem/SnapItem.jsx
+++ src/components/SnapItem/SnapItem.jsx
@@ -1,24 +1,21 @@
 import "./SnapItem.scss";
 
-function SnapItem(props){
-
-
-    return(
-       <li className="photo-card">
-         <img src={props.snap.photo} alt="" className="photo-card__photo"/>
-         <p className="photo-card__photographer">{props.snap.photographer}</p>
-         <ul className="photo-card__tags">
-            {props.snap.tags.map((tag,index) =>{
-                return(
-                    <li key= {index} className="photo-card__tag-item">
-                        {tag}
-                    </li>
-                )
-            })}
-         </ul>
-       </li>
-    );
+function SnapItem(props) {
+  return (
+    <li className="photo-card">
+      <img src={props.snap.photo} alt="" className="photo-card__img" />
+      <p className="photo-card__photographer">{props.snap.photographer}</p>
+      <ul className="photo-card__tags">
+        {props.snap.tags.map((tag, index) => {
+          return (
+            <li key={index} className="photo-card__tags--item">
+              {tag}
+            </li>
+          );
+        })}
+      </ul>
+    </li>
+  );
 }
 
 export default SnapItem;
-// console.log(SnapItem);
