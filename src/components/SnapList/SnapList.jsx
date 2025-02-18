7c397a5a5004a0478b617050c26536bfb6bc9785
Diya Sharma <sharmadiya7200@gmail.com>
2025-02-17 22:37:22 -0800

    Changes made in SnapList

--- src/components/SnapList/SnapList.jsx
+++ src/components/SnapList/SnapList.jsx
@@ -3,29 +3,26 @@ import "./SnapList.scss";
 import snapData from "../../data/photos.json";
 import SnapItem from "../SnapItem/SnapItem";
 
-function SnapList(props){
+function SnapList(props) {
+  const [snap, setSnap] = useState(snapData);
 
-    const [snap, setSnap] = useState(snapData);
+  let filteredSnap = snap;
 
-    let filteredSnap = snap;
+  if (props.selectedTag !== "") {
+    filteredSnap = snap.filter((snap) => {
+      return snap.tags.includes(props.selectedTag);
+    });
+  }
 
-    if (props.selectedTag !== "") {
-
-        filteredSnap = snap.filter((snap) =>{
-            return snap.tags.includes(props.selectedTag);
-        })
-    }
-
-
-    return(
-        <section className="snap-list">
-            <ul className="snap-list__info">
-                {filteredSnap.map((snap) =>{
-                    return <SnapItem key={snap.id} snap={snap} />;
-                })}
-            </ul>
-        </section>
-    );
+  return (
+    <section className="snap-list">
+      <ul className="snap-list__info">
+        {filteredSnap.map((snap) => {
+          return <SnapItem key={snap.id} snap={snap} />;
+        })}
+      </ul>
+    </section>
+  );
 }
 
 export default SnapList;
