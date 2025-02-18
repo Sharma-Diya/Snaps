d51ba542ac7076c7448de9bbbc4018e449c857f9
Diya Sharma <sharmadiya7200@gmail.com>
2025-02-17 22:22:24 -0800

    Changes made in FilterList

--- src/components/FilterList/FilterList.jsx
+++ src/components/FilterList/FilterList.jsx
@@ -1,28 +1,29 @@
 import "./FilterList.scss";
 import tagData from "../../data/tags.json";
 
-function FilterList(props){
-
-    return(
-        <section className="filter-list">
-            <ul className="filter-list__list">
-                {tagData.map((tag,index)=>{
-                    const highlight = tag === props.selectedTag;
-                    return (
-                        <li onClick={()=>{
-                            props.handleTagClick(tag);
-                        }}
-                        key = {index}
-                        className={`tag-list__item ${
-                            highlight === true ? "tag-list__item--selected" : null
-                          } `}
-                        >
-                          {tag}
-                        </li>
-                    );
-                })}
-            </ul>
-        </section>
-    )
+function FilterList(props) {
+  return (
+    <section className="filter">
+      <p>Filters</p>
+      <ul className="filter-list">
+        {tagData.map((tag, index) => {
+          const highlight = tag === props.selectedTag;
+          return (
+            <li
+              onClick={() => {
+                props.handleTagClick(tag);
+              }}
+              key={index}
+              className={`filter-list__item ${
+                highlight === true ? "filter-list__item--selected" : null
+              } `}
+            >
+              {tag}
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
 export default FilterList;
