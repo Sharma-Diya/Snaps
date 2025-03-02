e1254d9805909b35e579f657ade4a0a331a4d0c7
Diya Sharma <sharmadiya7200@gmail.com>
2025-03-01 20:14:00 -0800

    add api and an in-line error instead of an alert

--- src/components/AddComments/AddComments.jsx
+++ src/components/AddComments/AddComments.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import "./AddComments.scss";
-const apiKey = "e0eea5f0-0f8c-4b54-9fc4-ff50843766d4";
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
@@ -14,28 +13,20 @@ function AddComments({ fetchComments }) {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    console.log("added");
-
     if (!commentName || !commentDescription) {
-      setError(true);
-      alert("Please fill out both fields before submitting.");
+      setError("Please fill out both fields before submitting.");
       return;
     }
-    setError(false);
+    setError("");
 
     try {
-      const response = await axios.post(
-        `${backendUrl}/photos/${id}/comments`,
-        {
-          name: commentName,
-          comment: commentDescription,
-        }
-      );
+       await axios.post(`${backendUrl}/photos/${id}/comments`, {
+        name: commentName,
+        comment: commentDescription,
+      });
       fetchComments();
       setCommentName("");
       setCommentDescription("");
-
-      console.log("Comment added:", response.data);
     } catch (error) {
       console.error("Error adding comment:", error);
     }
@@ -67,6 +58,8 @@ function AddComments({ fetchComments }) {
             if (error) setError(false);
           }}
         ></textarea>
+
+        {error && <p className="error-message">{error}</p>}
 
         <button className="comment-form__button" type="submit">
           Submit
