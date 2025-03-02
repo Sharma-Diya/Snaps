16845ae482bddf7d543f049fcf58ce3a4ce5626c
Diya Sharma <sharmadiya7200@gmail.com>
2025-03-01 20:21:50 -0800

    add header on notfoundpage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,9 @@
+import Header from "../../components/Header/Header";
+
 function NotFoundPage() {
     return (
       <>
+        <Header page="not-found" />
         <h1>404</h1>
         <p>Can't find what you're looking for, sorry!</p>
       </>
